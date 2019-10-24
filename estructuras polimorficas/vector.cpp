1ebfa21daec68cfc9e88204267e7647bccff3777
ninjaoflight5 <jonguz5@hotmail.com>
2019-10-24 11:40:23 -0600

    implementado vector polimorfico

--- estructuras polimorficas/vector.cpp
+++ estructuras polimorficas/vector.cpp
@@ -1,1 +1,76 @@
 #include "vector.h"
+#include "objetoBase.h"
+
+
+
+vector::vector(size_t longitud) :
+	longitud(longitud), insertados(0)
+{
+	elementos = new objetoBase * [longitud];
+	for (size_t i = 0; i < longitud; i++) {
+		elementos[i] = nullptr;
+	}
+}
+
+
+size_t vector::getInsertados() const {
+	return insertados;
+}
+size_t vector::getLongitud() const {
+	return longitud;
+}
+
+
+
+
+
+
+void vector::insertar(objetoBase* nuevo) {
+	if (insertados == longitud) {
+		redimensionar(longitud);
+	}
+	elementos[insertados++] = nuevo;
+}
+bool vector::eliminar(const size_t& posicion) {
+	if (posicion < insertados){
+		delete elementos[posicion];
+		mover(posicion);
+		return true;
+	}
+	return false;
+}
+objetoBase* vector::obtener(size_t posicion) {
+	return posicion < insertados ? elementos[posicion] : nullptr;
+}
+
+
+
+void vector::redimensionar(size_t tam) {
+	size_t longitudNueva = longitud + tam;
+	objetoBase** nuevo = new objetoBase * [longitudNueva];
+
+	for (size_t i = 0; i < longitudNueva; i++){
+		nuevo[i] = obtener(i);
+	}
+
+	longitud = longitudNueva;
+	delete[] elementos;
+	elementos = nuevo;
+}
+void vector::mover(size_t posicion) {
+	for (size_t i = posicion; i < insertados - 1; i++) {
+		elementos[i] = elementos[i + 1];
+	}
+	elementos[--insertados] = nullptr;
+}
+
+
+
+vector::~vector() {
+	for (size_t i = 0; i < insertados; i++) {
+		delete elementos[i];
+		elementos[i] = nullptr;
+	}
+	delete[] elementos;
+	elementos = nullptr;
+}
--- estructuras polimorficas/vector.h
+++ estructuras polimorficas/vector.h
@@ -1,5 +1,32 @@
 #pragma once
-class vector
-{
-};
+class objetoBase;
+class vector {
+protected:
+	size_t longitud;
+	size_t insertados;
+	objetoBase** elementos;
+public:
+	vector(size_t longitud=15);
 
+
+
+	size_t getInsertados() const;
+	size_t getLongitud() const;
+
+	void insertar(objetoBase* nuevo);
+	bool eliminar(const size_t& posicion);
+
+	objetoBase* obtener(size_t posicion);
+
+	void redimensionar(size_t tam);
+
+private:
+
+	//mueve los elementos posteriores hacia atras desde la posicion,
+	//util cuando se elimina en el inicio o el medio
+	void mover(size_t posicion);
+	
+public:
+	~vector();
+
+};
